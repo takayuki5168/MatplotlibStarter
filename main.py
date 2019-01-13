16d153e5ca27741f556180cbdfe9f6c004096c82
Takayuki Murooka <takayuki5168@gmail.com>
2019-01-13 17:54:39 +0900

    some changes

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
+import sys, signal
+
 class MetaData:
-    def __init__(self, file_name, rows, label=""):
+    def __init__(self, file_name, rows, label="", color=None, start=None, lambda_function=[lambda x:x, lambda x:x]):
         self.file_name = file_name
-        self.rows = rows
+        self.rows = rows    # rows of x, y in file
         self.label = label
+        self.color = color
+        self.start = start   # plot from x after self.start
+        self.lambda_function = lambda_function   # convert x, y raw values in file, with your customized function
         
 class MatplotlibTemplate:
-    def __init__(self, vsplit, hsplit, metadata_list, real_time=False, split_char=" "):
+    def __init__(self, vsplit, hsplit, metadata_list, titles_list, real_time=False, split_char=" "):
         self.fig = plt.figure(figsize=(6, 4))
         
         self.axes = [[0 for h in range(hsplit)] for v in range(vsplit)]
@@ -16,36 +21,65 @@ class MatplotlibTemplate:
         for v in range(vsplit):
             for h in range(hsplit):
                 self.axes[v][h] = self.fig.add_subplot(vsplit, hsplit, h + 1 + v * hsplit)
+                self.axes[v][h].set_title(titles_list[v][h][0])
+                self.axes[v][h].set_xlabel(titles_list[v][h][1])
+                self.axes[v][h].set_ylabel(titles_list[v][h][2])
                 for metadata_idx in range(len(metadata_list[v][h])):
                     metadata = metadata_list[v][h][metadata_idx]
+                    lambda_x = metadata.lambda_function[0]
+                    lambda_y = metadata.lambda_function[1]
+                    label = metadata.label
+                    color = metadata.color
+                    start = metadata.start
+                    
                     with open(metadata.file_name, "r") as f:
-                        x = []
-                        y = []
+                        x_list = []
+                        y_list = []
                         cnt = 0
                         for line in f:
                             number_vector = line[:-1].split(split_char)
                             if number_vector == ["\n"] or number_vector == [""]:
                                 continue
+                            
                             if metadata.rows[0] == -1:
-                                x.append(cnt)
+                                x = lambda_x(cnt)
                             else:
-                                x.append(float(number_vector[metadata.rows[0]]))
-                            y.append(float(number_vector[metadata.rows[1]]))
+                                x = lambda_x(float(number_vector[metadata.rows[0]]))
+                            y = lambda_y(float(number_vector[metadata.rows[1]]))
                             cnt += 1
-                        self.lines[v][h][metadata_idx], = self.axes[v][h].plot(np.array(x), np.array(y))#, label[v][h][i])
+
+                            if start == None or x < start:
+                                continue
+                            
+                            x_list.append(x)                                
+                            y_list.append(y)
+                            
+                        print(label)
+                        if color == None:
+                            self.lines[v][h][metadata_idx], = self.axes[v][h].plot(np.array(x_list), np.array(y_list), label=metadata.label)
+                        else:
+                            self.lines[v][h][metadata_idx], = self.axes[v][h].plot(np.array(x_list), np.array(y_list), label=metadata.label, color=color)
+                self.axes[v][h].legend()
 
     def execute(self):
         plt.show()
     
 if __name__ == "__main__":
-    a = [MetaData("/home/takayuki/ros/kinetic/src/jsk_diabolo_pr2/log/log-by-logger/sample_log_with_robust.log", [-1, 2], "Pitch"),
-         MetaData("/home/takayuki/ros/kinetic/src/jsk_diabolo_pr2/log/log-by-logger/sample_log_with_robust.log", [-1, 3], "Yaw")]
-    b = [MetaData("/home/takayuki/ros/kinetic/src/jsk_diabolo_pr2/log/log-by-logger/sample_log.log", [-1, 0], "u1"),
-         MetaData("/home/takayuki/ros/kinetic/src/jsk_diabolo_pr2/log/log-by-logger/sample_log.log", [-1, 1], "u2")]
+    signal.signal(signal.SIGINT, lambda signal, frame: sys.exit(0))
+    
+    ax1 = [MetaData("/home/takayuki/ros/kinetic/src/jsk_diabolo_pr2/log/log-by-logger/sample_log_with_robust.log", [-1, 2], label="Pitch", color="blue", start=3200),
+         MetaData("/home/takayuki/ros/kinetic/src/jsk_diabolo_pr2/log/log-by-logger/sample_log_with_robust.log", [-1, 3], label="Yaw", color="green", start=3200)]
+    ax2 = [MetaData("/home/takayuki/ros/kinetic/src/jsk_diabolo_pr2/log/log-by-logger/sample_log_with_robust.log", [-1, 0], label="u1", color="purple", start=3200, lambda_function=[lambda x:x, lambda x:x-0.7]),
+         MetaData("/home/takayuki/ros/kinetic/src/jsk_diabolo_pr2/log/log-by-logger/sample_log_with_robust.log", [-1, 1], label="u2", color="yellow", start=3200, lambda_function=[lambda x:x, lambda x:x*20])]
+
+    ax1_title = ["State of Diabolo", "time", ""]
+    ax2_title = ["Input from Roobot", "time", ""]
     
     plotter = MatplotlibTemplate(2, 1,
-                                 [[a],
-                                  [b]])
+                                 [[ax1],
+                                  [ax2]],
+                                 [[ax1_title],
+                                  [ax2_title]])
 
     '''
     plotter = MatplotlibTemplate(3, 2,
